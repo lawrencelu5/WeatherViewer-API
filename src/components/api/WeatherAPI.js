20b1b406cfb99db7695f7122a1b03f46df1d48dd
Lawrence Lu <lawrencelu18@gmail.com>
2019-06-27 18:08:58 -0700

    replaced FontAwesome icons with weatherAPI's default icons

--- src/components/api/WeatherAPI.js
+++ src/components/api/WeatherAPI.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCloud } from '@fortawesome/free-solid-svg-icons';
 var API_KEY = "";
 var BASE_URL = "https://api.openweathermap.org/data/2.5/weather?id=6173331&units=metric&appid=" + API_KEY;
+var TIME_SECONDS = 1000;
 
 export class WeatherAPI extends Component {
   displayName = WeatherAPI.name
@@ -12,7 +12,9 @@ export class WeatherAPI extends Component {
     super();
     this.state = {
         weatherData: [],
-        celsius: 0
+        celsius: 0,
+        weatherIcon: '',
+        localTime: ''
     };
         this.getData = this.getData.bind(this);
         this.celsiusConvert = this.celsiusConvert.bind(this);
@@ -21,6 +23,7 @@ export class WeatherAPI extends Component {
   // Called when constructor is finished building component.
   componentDidMount() {
     this.getData();
+    this.timer = setInterval (() => this.getData(), 600000);
   }
 
   getData() {
@@ -29,6 +32,8 @@ export class WeatherAPI extends Component {
             this.setState({weatherData: data});
             console.log(JSON.stringify(data));
             this.setState({celsius: data.main.temp});
+            this.setState({localTime: new Date(data.dt * TIME_SECONDS)});
+            this.setState({weatherIcon: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`})
         })
         .catch((error) => {
             console.log(error);
@@ -47,9 +52,12 @@ export class WeatherAPI extends Component {
       </div>
       <div className="col">
         <div className="card" style={{width: 18 + "rem"}}>
-            <div className="card-img-top text-center"> <FontAwesomeIcon icon={faCloud} size="9x" /> </div>
+            <div className="card-header">
+              <h5 className="card-title text-center">{this.state.weatherData.name}</h5>
+            </div>
+            <div className="card-img-top text-center"> <img src={this.state.weatherIcon} /> </div>
             <div className="card-body">
-                <h5 className="card-title">{this.state.weatherData.name}</h5>
+                <p className="card-text">{this.state.localTime.toString()}</p> 
                 <p className="card-text">It feels like <b>{this.state.celsius}°C</b>.</p>
                 <p className="card-text">It looks like {this.state.weatherData.weather[0].description}.</p>
             </div>
